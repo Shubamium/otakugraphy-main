5c8e16a342c7bd06e6ab869f8168d79e44988dc6
Vincent_Liussyaputra <liusvinv@gmail.com>
2026-02-18 12:43:39 +0700

    Add gated to otgdiff

--- src/app/otg-difference/page.tsx
+++ src/app/otg-difference/page.tsx
@@ -5,6 +5,7 @@ import "./otgdiff.scss";
 
 import { fetchData, urlFor } from "../db/sanity";
 import OTGDiff from "./OTGDiff";
+import GateClient from "../components/passwordGate/GateClient";
 // import UnderConstruction from "../components/underConstuction/UnderConstruction";
 export default async function Page({}: Props) {
   const gd = await fetchData<any>(`
@@ -16,7 +17,6 @@ export default async function Page({}: Props) {
   const otgdiff = await fetchData<any>(`
 			*[_type == 'otg-diff' && preset == 'main']{
 				...,
-				frames,
 			}[0]
 		`);
   const framesList = await fetchData<any>(`
@@ -25,5 +25,11 @@ export default async function Page({}: Props) {
 			}
 	`);
 
-  return <OTGDiff otgdiff={otgdiff} fl={framesList} gd={gd}></OTGDiff>;
+  return otgdiff.gated === true && otgdiff.gated_pass ? (
+    <GateClient gated_pass={otgdiff.gated_pass}>
+      <OTGDiff otgdiff={otgdiff} fl={framesList} gd={gd}></OTGDiff>
+    </GateClient>
+  ) : (
+    <OTGDiff otgdiff={otgdiff} fl={framesList} gd={gd}></OTGDiff>
+  );
 }
