df27c08f30b325cf7486efdc9ad9ccf7f7e8f52c
Vincent_Liussyaputra <liusvinv@gmail.com>
2026-05-07 23:00:31 +0700

    Remove client upload

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -43,7 +43,6 @@ export default buildConfig({
   plugins: [
     s3Storage({
       bucket: "otakugraphy",
-      clientUploads: true,
       collections: {
         media: {
           disablePayloadAccessControl: true,
